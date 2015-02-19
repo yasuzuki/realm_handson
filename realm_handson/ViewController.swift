c19dfdc206cced4016fd071a37be92cc632dd3ff
Yasunori Suzuki <railsyas@gmail.com>
2015-02-19 23:23:19 +0900

    Initial Commit

---
+++ realm_handson/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  realm_handson
+//
+//  Created by yas on 2015/02/19.
+//  Copyright (c) 2015年 yasuzuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
